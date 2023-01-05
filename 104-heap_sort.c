415ae91b76de7108359f9c47b2caaf27292e01cd
LCobham <lucascobham@hotmail.com>
2023-01-05 11:33:22 -0800

    Fixed bugs in heapsort and big o file

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -15,7 +15,7 @@ void heap_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	for (heapSize = size; heapSize > 0; heapSize--)
+	for (heapSize = size; heapSize > 1; heapSize--)
 	{
 		for (i = heapSize / 2 - 1; (int) i >= 0; i--)
 			SiftDown(array, i, heapSize, size);
